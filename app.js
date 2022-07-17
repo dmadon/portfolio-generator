2b64f8195af8556ff3f14f24d7499e13dea4b4ff
dmadon <deanna.madon@gmail.com>
2022-07-17 15:18:04 -0500

    generate HTML

--- app.js
+++ app.js
@@ -1,17 +1,27 @@
+const fs=require('fs');
+const generatePage = require('./src/page-template.js');
 const profileDataArgs = process.argv.slice(2, process.argv.length);
-console.log(profileDataArgs);
+const [name,github]=profileDataArgs;
+// console.log(profileDataArgs);
 
-const printProfileData = profileDataArr=>{
+// const printProfileData = profileDataArr=>{
 
-    // This...
-    for(let i=0;i<profileDataArr.length;i++){
-        console.log(profileDataArr[i]);
-    }
+//     // This...
+//     for(let i=0;i<profileDataArr.length;i++){
+//         console.log(profileDataArr[i]);
+//     }
 
-    console.log("=====================");
+//     console.log("=====================");
 
-    // is the same as this...
-    profileDataArr.forEach(profileItem=>console.log(profileItem));
-    };
+//     // is the same as this...
+//     profileDataArr.forEach(profileItem=>console.log(profileItem));
+//     };
 
-printProfileData(profileDataArgs);
+// printProfileData(profileDataArgs);
+
+
+fs.writeFile('./index.html',generatePage(name,github),err=>{
+    if(err) throw err;
+    console.log('Portfolio complete! Check out index.html to see the output!')
+
+});
